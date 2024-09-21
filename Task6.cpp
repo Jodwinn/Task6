2a3830686c0913c97e66a9dc05b3c54689d75c33
Jodwinn <pavelkharkovski@gmail.com>
2024-09-21 14:53:17 +0300

    Create Task6.cpp

---
+++ Task6.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+int main() {
+	int glass = 5;
+	int plate = 5;
+	int knife = 5;
+	int table = glass + plate + knife;
+	cout << table;
+}
